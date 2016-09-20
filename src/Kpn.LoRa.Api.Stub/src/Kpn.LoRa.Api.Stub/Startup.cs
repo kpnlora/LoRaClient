b4b9191b969fda596115b1c1d981b22de4f1ade9
Rick van den Bosch <r.van.den.bosch@betabit.nl>
2016-09-20 19:42:42 +0200

    Revert "Test commit"
    This reverts commit c100c1400a352e5f255b3e2a66c9614b82c07daf.

--- src/Kpn.LoRa.Api.Stub/src/Kpn.LoRa.Api.Stub/Startup.cs
+++ src/Kpn.LoRa.Api.Stub/src/Kpn.LoRa.Api.Stub/Startup.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-
+using Swashbuckle.Application;
 
 namespace Kpn.LoRa.Api.Stub
 {
